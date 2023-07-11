591b4b00f3f739cdaf410e2d82e99d7909d70949
anisbastide <anis.bastide@odigo.com>
2023-07-11 23:50:31 +0200

    finished details

--- src/app/country-details/country-details.component.ts
+++ src/app/country-details/country-details.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Continent, Country} from "../utils/type";
+import {ActivatedRoute} from "@angular/router";
+import {CountryTableComponent} from "../country-table/country-table.component";
 
 @Component({
   selector: 'app-country-details',
@@ -8,7 +10,9 @@ import {Continent, Country} from "../utils/type";
 })
 export class CountryDetailsComponent implements OnInit {
   public country:Country =  {GDP: 0, continent: Continent.Europe, image: "", name: "", population: 0, size: 0}
-  constructor() { }
+  constructor(private route:ActivatedRoute,private countryTableComponent:CountryTableComponent) {
+    this.route.params.subscribe( params => this.country = countryTableComponent.countries[params['id']] );
+  }
 
   ngOnInit(): void {
   }
--- src/app/details-header/details-header.component.ts
+++ src/app/details-header/details-header.component.ts
@@ -12,11 +12,9 @@ export class DetailsHeaderComponent implements OnInit {
   public country:Country = {GDP: 0, continent: Continent.Europe, image: "", population: 0, size: 0, name:'France'}
 
   constructor(private route:ActivatedRoute,private countryTableComponent:CountryTableComponent) {
-    let id = 0
     this.route.params.subscribe( params => this.country = countryTableComponent.countries[params['id']] );
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
