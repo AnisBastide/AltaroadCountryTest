ec476925575e41d3d4e85c2a5f773ba48b921d75
anisbastide <anis.bastide@odigo.com>
2023-07-13 01:38:35 +0200

    revert

--- src/app/country-modal/country-modal.component.ts
+++ src/app/country-modal/country-modal.component.ts
@@ -57,7 +57,6 @@ export class CountryModalComponent implements OnInit {
   }
   public updateImage(event:Event){
     this.image = (event.target as HTMLInputElement).value
-    console.log(this.image);
   }
 
   public loadCountry(country:Country){
